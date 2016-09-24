0023ffd8b9b23b2f87c9a8e1cb8baab02cc8e7f7
ovidiujurje <ovidiujurje@gmail.com>
2016-09-24 15:08:37 +0300

    Calculate rate for the second month

--- JuniorMind/BankRate/BankRateTests.cs
+++ JuniorMind/BankRate/BankRateTests.cs
@@ -12,12 +12,18 @@ namespace BankRate
             decimal rate = CalculateBankRate(200, 2, 12, 1);
             Assert.AreEqual(102, rate);
         }
+        [TestMethod]
+        public void RateForSecondMonth()
+        {
+            decimal rate = CalculateBankRate(200, 2, 12, 2);
+            Assert.AreEqual(101, rate);
+        }
         decimal CalculateBankRate(decimal total, int periodInMonths, decimal interestPerYear, int currentMonth)
-
         {
             decimal principal = total / periodInMonths;
             decimal exactInterestPerMonth = interestPerYear / 12 / 100;
-            return principal + total * exactInterestPerMonth;
+            decimal sold = total - (currentMonth - 1) * principal;
+            return principal + sold * exactInterestPerMonth;
         }
     }
 }
