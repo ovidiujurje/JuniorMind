5026ff56e5d036d963d79799827620dce63e0492
ovidiujurje <ovidiujurje@gmail.com>
2016-09-24 18:46:24 +0300

    Calculate the distance travelled by bird for an initial distance of 200

--- JuniorMind/Trains/TrainsTests.cs
+++ JuniorMind/Trains/TrainsTests.cs
@@ -4,11 +4,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Trains
 {
     [TestClass]
-    public class UnitTest1
+    public class TrenuriTests
     {
         [TestMethod]
         public void TestMethod1()
         {
+            float distance = CalculateDistanceCoveredByBird(200);
+            Assert.AreEqual(150, distance);
+        }
+        float CalculateDistanceCoveredByBird(float initialDistance)
+        {
+            return initialDistance - initialDistance / 4;
         }
     }
 }
