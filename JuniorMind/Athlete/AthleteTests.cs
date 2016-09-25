b6c8157ef747a75f96949dc2f2658efe80198d82
ovidiujurje <ovidiujurje@gmail.com>
2016-09-25 19:06:45 +0300

    Calculate number of repetitions for one hundred rounds

--- JuniorMind/Athlete/AthleteTests.cs
+++ JuniorMind/Athlete/AthleteTests.cs
@@ -12,6 +12,13 @@ namespace Athlete
             uint repetitions = CalculateTotalNumberOfRepetitions(5);
             Assert.AreEqual(25u, repetitions);
         }
+        [TestMethod]
+        public void TotalNumberOfRepetitionsForOneHundredRounds()
+        {
+            uint repetitions = CalculateTotalNumberOfRepetitions(100);
+            Assert.AreEqual(10000u, repetitions);
+        }
+
         uint CalculateTotalNumberOfRepetitions(uint nRounds)
         {
             return nRounds * nRounds;
