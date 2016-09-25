052d72f78ee72b6609f558bb4d5d12e1d8fef7cb
ovidiujurje <ovidiujurje@gmail.com>
2016-09-25 20:12:48 +0300

    Calculate number of 40X20 parquet pieces required to cover a 68X51 room

--- JuniorMind/Parquet/ParquetTests.cs
+++ JuniorMind/Parquet/ParquetTests.cs
@@ -4,11 +4,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Parquet
 {
     [TestClass]
-    public class UnitTest1
+    public class ParquetTests
     {
         [TestMethod]
         public void TestMethod1()
         {
+            float parquet = CalculateNumberOfParquetPiecesRequiredForRoom(68, 51, 40, 20);
+            Assert.AreEqual(6, parquet);
+        }
+        float CalculateNumberOfParquetPiecesRequiredForRoom(float nRoomLength, float mRoomWidth, float aParquetPieceLength, float bParquetPieceWidth)
+        {
+            return nRoomLength / (aParquetPieceLength - 0.15f * aParquetPieceLength) * mRoomWidth / (bParquetPieceWidth - 0.15f * bParquetPieceWidth);
         }
     }
 }
