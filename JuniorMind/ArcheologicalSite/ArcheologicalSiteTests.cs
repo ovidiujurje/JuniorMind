f768a6545b2345faf2e931f69ebf645f2dd9d3b6
ovidiujurje <ovidiujurje@gmail.com>
2016-09-25 21:49:26 +0300

    Calculate minimum building area for condition 1
    Coordinates:
    Column A - 1 latitude, 1 longitude
    Column B - 2 latitude, 2 longitude
    Column C - 3 latitude, 3 longitude

--- JuniorMind/ArcheologicalSite/ArcheologicalSiteTests.cs
+++ JuniorMind/ArcheologicalSite/ArcheologicalSiteTests.cs
@@ -4,11 +4,21 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ArcheologicalSite
 {
     [TestClass]
-    public class UnitTest1
+    public class ArcheologicalSiteTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void MinimumAreaCondition1()
         {
+            double area = CalculateMinimumBuildingArea(1, 1, 2, 2, 3, 3);
+            Assert.AreEqual(0, area);
+        }
+        double CalculateMinimumBuildingArea(float latitudeColumnA, float longitudeColumnA, float latitudeColumnB, float longitudeColumnB, float latitudeColumnC, float longitudeColumnC)
+        {
+            double distanceFromAToB = Math.Sqrt((latitudeColumnA - latitudeColumnB) * (latitudeColumnA - latitudeColumnB) + (longitudeColumnA - longitudeColumnB) * (longitudeColumnA - longitudeColumnB));
+            double distanceFromBToC = Math.Sqrt((latitudeColumnB - latitudeColumnC) * (latitudeColumnB - latitudeColumnC) + (longitudeColumnB - longitudeColumnC) * (longitudeColumnB - longitudeColumnC));
+            double distanceFromCtoA = Math.Sqrt((latitudeColumnC - latitudeColumnA) * (latitudeColumnC - latitudeColumnA) + (longitudeColumnC - longitudeColumnA) * (longitudeColumnC - longitudeColumnA));
+            double semiPerimeter = (distanceFromAToB + distanceFromBToC + distanceFromCtoA) / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - distanceFromAToB) * (semiPerimeter - distanceFromBToC) * (semiPerimeter - distanceFromCtoA));
         }
     }
 }
