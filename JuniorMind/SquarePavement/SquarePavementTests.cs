50bfed9e0f8968b240508d7a745a027f6eb75ed1
ovidiujurje <ovidiujurje@gmail.com>
2016-09-25 17:25:03 +0300

    Calculate number of 5X5 cubic stones required to cover 22X29 square

--- JuniorMind/SquarePavement/SquarePavementTests.cs
+++ JuniorMind/SquarePavement/SquarePavementTests.cs
@@ -12,6 +12,13 @@ namespace SquarePavement
             double stones = CalculateCubicStonesRequiredToPaveSquare(9, 8, 4);
             Assert.AreEqual(6, stones);
         }
+        [TestMethod]
+        public void NumberOfFiveByFiveStonesRequiredForTwentyTwoByTwentyNineSquare()
+        {
+            double stones = CalculateCubicStonesRequiredToPaveSquare(22, 29, 5);
+            Assert.AreEqual(30, stones);
+        }
+
         double CalculateCubicStonesRequiredToPaveSquare(float mSquareLength, float nSquareWidth, float aStoneLengthWidth)
         {
             double stonesRequiredLengthwise = Math.Ceiling(mSquareLength / aStoneLengthWidth);
