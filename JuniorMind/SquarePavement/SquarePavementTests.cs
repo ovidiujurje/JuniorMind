362c923c96d6a9aa80b8b297029b167caeab283c
ovidiujurje <ovidiujurje@gmail.com>
2016-09-25 17:00:35 +0300

    Calculate number of 4X4 cubic stones required to cover a 9X8 Square

--- JuniorMind/SquarePavement/SquarePavementTests.cs
+++ JuniorMind/SquarePavement/SquarePavementTests.cs
@@ -4,11 +4,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace SquarePavement
 {
     [TestClass]
-    public class UnitTest1
+    public class SquarePavementTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void NumberOfFourByFourStonesRequiredForNineByEightSquare()
         {
+            double stones = CalculateCubicStonesRequiredToPaveSquare(9, 8, 4);
+            Assert.AreEqual(6, stones);
+        }
+        double CalculateCubicStonesRequiredToPaveSquare(float mSquareLength, float nSquareWidth, float aStoneLengthWidth)
+        {
+            return Math.Ceiling(mSquareLength / aStoneLengthWidth) * Math.Ceiling(nSquareWidth / aStoneLengthWidth);
         }
     }
 }
