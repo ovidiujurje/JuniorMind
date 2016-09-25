743ffcd9e1e48e2e409046502bae8249fa4414d3
ovidiujurje <ovidiujurje@gmail.com>
2016-09-25 14:36:53 +0300

    Calculate number of red mushrooms for three total and twice as many red than white

--- JuniorMind/Mushrooms/MushroomsTests.cs
+++ JuniorMind/Mushrooms/MushroomsTests.cs
@@ -4,11 +4,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Mushrooms
 {
     [TestClass]
-    public class UnitTest1
+    public class MushroomsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RedMushroomsForThreeTotalAndTwiceAsManyRedThanWhite()
         {
+            float redMushrooms = CalculateNumberOfRedMushrooms(3, 2);
+            Assert.AreEqual(2, redMushrooms);
+        }
+        float CalculateNumberOfRedMushrooms(uint nMushrooms, float xMoreRedThanWhiteMushrooms)
+        {
+            return xMoreRedThanWhiteMushrooms * nMushrooms / (xMoreRedThanWhiteMushrooms + 1);
         }
     }
 }
