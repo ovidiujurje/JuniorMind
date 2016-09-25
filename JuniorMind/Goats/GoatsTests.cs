f33cfaf6b135c1dfe21afa3462dc18406a56e69f
ovidiujurje <ovidiujurje@gmail.com>
2016-09-25 13:33:21 +0300

    Extract days increase, goats increase and hay increase variables

--- JuniorMind/Goats/GoatsTests.cs
+++ JuniorMind/Goats/GoatsTests.cs
@@ -14,7 +14,10 @@ namespace Goats
         }
         float CalculateHayEatenByQGoatsInWDays(float xDays, uint yGoats, float zKgOfHay, float wDays, uint qGoats)
         {
-            return wDays / xDays * qGoats / yGoats * zKgOfHay;
+            float factorIncreaseInDays = wDays / xDays;
+            float factorIncreaseInGoats = qGoats / yGoats;
+            float factorIncreaseInHay = factorIncreaseInDays * factorIncreaseInGoats;
+            return factorIncreaseInHay * zKgOfHay;
         }
     }
 }
