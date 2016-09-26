d3cf76cbe50f001312930e9e60bfb53279ffa066
ovidiujurje <ovidiujurje@gmail.com>
2016-09-26 21:33:49 +0300

    Calculate total debt if I am five days late

--- JuniorMindSet2/Debt/DebtTests.cs
+++ JuniorMindSet2/Debt/DebtTests.cs
@@ -4,11 +4,16 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Debt
 {
     [TestClass]
-    public class UnitTest1
+    public class DebtTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RentFiveDaysLate()
         {
+            Assert.AreEqual(110, CalculateTotalDebt(100, 5));
+        }
+        decimal CalculateTotalDebt(decimal rent, int daysLate)
+        {
+            return rent + rent * 0.02m * daysLate ;
         }
     }
 }
