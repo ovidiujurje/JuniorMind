5f9c4481e88e0f66c2e5208f4c902cdc11efbcbf
ovidiujurje <ovidiujurje@gmail.com>
2016-09-26 21:42:57 +0300

    Calculate total debt if I am 20 days late

--- JuniorMindSet2/Debt/DebtTests.cs
+++ JuniorMindSet2/Debt/DebtTests.cs
@@ -11,10 +11,16 @@ namespace Debt
         {
             Assert.AreEqual(110, CalculateTotalDebt(100, 5));
         }
+        [TestMethod]
+        public void RentTwentyDaysLate()
+        {
+            Assert.AreEqual(200, CalculateTotalDebt(100, 20));
+        }
         decimal CalculateTotalDebt(decimal rent, int daysLate)
         {
-            decimal penalty = rent * 0.02m * daysLate;
-            return rent +  penalty;
+            decimal percentPenalty = daysLate > 10 ? 0.05m : 0.02m;
+            decimal penalty = rent * percentPenalty * daysLate;
+            return rent + penalty;
         }
     }
 }
