9372a8d950d357bfb9671ea0960a31de66bc018c
ovidiujurje <ovidiujurje@gmail.com>
2016-09-26 16:42:47 +0300

    Calculate daytime fare for medium distances

--- JuniorMindSet2/TaxiFare/TaxiFareTests.cs
+++ JuniorMindSet2/TaxiFare/TaxiFareTests.cs
@@ -9,12 +9,17 @@ namespace TaxiFare
         [TestMethod]
         public void DaytimeFareForShortDistances()
         {
-            decimal fare = CalculateTaxiFare(1, 8);
-            Assert.AreEqual(5, fare);
+            Assert.AreEqual(5, CalculateTaxiFare(1, 8));
         }
+        [TestMethod]
+        public void DaytimeFareForMediumDistances()
+        {
+            Assert.AreEqual(168, CalculateTaxiFare(21, 8));
+        }
+
         decimal CalculateTaxiFare(int distanceInKm, int hour)
         {
-            decimal pricePerKm = 5;
+            decimal pricePerKm = distanceInKm > 20 ? 8 : 5;
             return distanceInKm * pricePerKm;
         }
     }
