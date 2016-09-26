bc40da88aa798580beede25fbcc8d0ef35519b0c
ovidiujurje <ovidiujurje@gmail.com>
2016-09-26 16:29:22 +0300

    Calculate daytime taxi fare for short distance

--- JuniorMindSet2/TaxiFare/TaxiFareTests.cs
+++ JuniorMindSet2/TaxiFare/TaxiFareTests.cs
@@ -4,11 +4,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace TaxiFare
 {
     [TestClass]
-    public class UnitTest1
+    public class TaxiFareTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void DaytimeFareForShortDistances()
         {
+            decimal fare = CalculateTaxiFare(1, 8);
+            Assert.AreEqual(5, fare);
+        }
+        decimal CalculateTaxiFare(int distanceInKm, int hour)
+        {
+            return distanceInKm * 5;
         }
     }
 }
