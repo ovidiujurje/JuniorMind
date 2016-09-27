e55bcd60a45c3fd364f625ebb034976f5a7dac86
ovidiujurje <ovidiujurje@gmail.com>
2016-09-27 13:10:35 +0300

    Calculate area of initial terrain

--- JuniorMindSet2/FarmerTerrain/FarmerTerrainTests.cs
+++ JuniorMindSet2/FarmerTerrain/FarmerTerrainTests.cs
@@ -4,11 +4,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace FarmerTerrain
 {
     [TestClass]
-    public class UnitTest1
+    public class FarmerTerrainTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CorrectArea()
         {
+            Assert.AreEqual(592900, CalculateAreaOfInitialTerrain(770000, 230));
+        }
+        double CalculateAreaOfInitialTerrain(double areaOfNewTerrain, double widthOfRectangle)
+        {
+            double delta = widthOfRectangle * widthOfRectangle - 4 * (-areaOfNewTerrain);
+            double initialLength = (-widthOfRectangle + Math.Sqrt(delta)) / 2;
+            return initialLength * initialLength;
         }
     }
 }
