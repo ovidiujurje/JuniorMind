20528ecc998daa51db4158c357838ed56b2d1836
ovidiujurje <ovidiujurje@gmail.com>
2016-09-29 00:29:04 +0300

    Get one-letter column name

--- JuniorMindSet3/ExcelColumns/ExcelColumns.cs
+++ JuniorMindSet3/ExcelColumns/ExcelColumns.cs
@@ -4,11 +4,25 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ExcelColumns
 {
     [TestClass]
-    public class UnitTest1
+    public class ExcelColumns
     {
         [TestMethod]
-        public void TestMethod1()
+        public void OneLetter()
         {
+            Assert.AreEqual("C", GetColumnCharacterCombination(3));
+        }
+        string GetColumnCharacterCombination(int columnNumber)
+        {
+            string columnLetters = string.Empty;
+            char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            int i = 0;
+            while (columnNumber > 0)
+            {
+                i = (columnNumber - 1) % 26;
+                columnLetters = alphabet[i] + columnLetters;
+                columnNumber = ((columnNumber - i) / 26);
+            }
+            return columnLetters;
         }
     }
 }
