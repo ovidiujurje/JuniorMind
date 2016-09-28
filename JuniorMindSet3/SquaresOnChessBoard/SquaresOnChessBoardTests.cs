43e277dc707513fe6b15bed41842b6406d3fc0ab
ovidiujurje <ovidiujurje@gmail.com>
2016-09-28 19:46:45 +0300

    Calculate squares on 8X8 board

--- JuniorMindSet3/SquaresOnChessBoard/SquaresOnChessBoardTests.cs
+++ JuniorMindSet3/SquaresOnChessBoard/SquaresOnChessBoardTests.cs
@@ -4,11 +4,21 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace SquaresOnChessBoard
 {
     [TestClass]
-    public class UnitTest1
+    public class SquaresOnChessBoardTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void NumberOfSquaresOnEightByEightBoard()
         {
+            Assert.AreEqual(204, CalculateNumberOfSquares(8, 8));
+        }
+        int CalculateNumberOfSquares(int length, int width)
+        {
+            int numberOfPossibleSquares = 0;
+            for (int i = 1; i <= length; i++)
+            {
+                numberOfPossibleSquares += i*i;
+            }
+            return numberOfPossibleSquares;
         }
     }
 }
