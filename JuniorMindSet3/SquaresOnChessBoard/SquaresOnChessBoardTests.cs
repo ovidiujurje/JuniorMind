f3be499ef5034e3357d17b9ca4eb895ecc802696
ovidiujurje <ovidiujurje@gmail.com>
2016-09-28 19:49:53 +0300

    Calculate number of squares for boards with different length and width

--- JuniorMindSet3/SquaresOnChessBoard/SquaresOnChessBoardTests.cs
+++ JuniorMindSet3/SquaresOnChessBoard/SquaresOnChessBoardTests.cs
@@ -11,14 +11,25 @@ namespace SquaresOnChessBoard
         {
             Assert.AreEqual(204, CalculateNumberOfSquares(8, 8));
         }
+        [TestMethod]
+        public void NumberOfSquaresOnDifferentLengthAndWidthBoard()
+        {
+            Assert.AreEqual(20, CalculateNumberOfSquares(4, 3));
+        }
+
         int CalculateNumberOfSquares(int length, int width)
         {
             int numberOfPossibleSquares = 0;
-            for (int i = 1; i <= length; i++)
+            int i = length - width + 1;
+            int j = 1;
+            while (i <= length && j <= width)
             {
-                numberOfPossibleSquares += i*i;
+                numberOfPossibleSquares += i * j;
+                i += 1;
+                j += 1;
             }
             return numberOfPossibleSquares;
         }
     }
 }
+
