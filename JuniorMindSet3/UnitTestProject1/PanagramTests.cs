8cdd4accbd942bb43de59b8d83ba2ad9ce08288d
ovidiujurje <ovidiujurje@gmail.com>
2016-09-28 22:43:48 +0300

    Replace for with foreach

--- JuniorMindSet3/UnitTestProject1/PanagramTests.cs
+++ JuniorMindSet3/UnitTestProject1/PanagramTests.cs
@@ -20,9 +20,9 @@ namespace UnitTestProject1
         {
             bool notAPanagram = false;
             char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-            for (int i = 0; i <= 25; i++)
+            foreach (char letter in alphabet)
             {
-                int este = phrase.IndexOf(alphabet[i]);
+                int este = phrase.IndexOf(letter);
                 if (este == -1)
                 {
                     notAPanagram = true;
