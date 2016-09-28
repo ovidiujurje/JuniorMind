f62bbb8b2df5c7f5e1520586cee7277b54c7670c
ovidiujurje <ovidiujurje@gmail.com>
2016-09-28 21:32:07 +0300

    Get prefix for words

--- JuniorMindSet3/Prefix/PrefixTests.cs
+++ JuniorMindSet3/Prefix/PrefixTests.cs
@@ -4,11 +4,29 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Prefix
 {
     [TestClass]
-    public class UnitTest1
+    public class PrefixTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShortWord()
         {
+            Assert.AreEqual("mag", GetPrefix("magenta", "magnific"));
+        }
+        [TestMethod]
+        public void LongerWord()
+        {
+            Assert.AreEqual("abcdefgh", GetPrefix("abcdefghzhjb", "abcdefghweoriguweiogtu"));
+        }
+        string GetPrefix(string firstWord, string secondWord)
+        {
+            string prefix = string.Empty;
+            for (int i = 1; i <= firstWord.Length && i <= secondWord.Length; i++)
+            {
+            if (firstWord.Substring(0, i) == secondWord.Substring(0, i))
+            continue;
+            prefix += firstWord.Substring(0, i - 1);
+            break;
+            }
+            return prefix;
         }
     }
 }
