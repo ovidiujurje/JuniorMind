461d2a98791202934168bf4a7cdac4301f837abf
ovidiujurje <ovidiujurje@gmail.com>
2016-09-29 20:54:00 +0300

    Calculate first number

--- JuniorMindSet3/Cube/CubeTests.cs
+++ JuniorMindSet3/Cube/CubeTests.cs
@@ -4,11 +4,33 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Cube
 {
     [TestClass]
-    public class UnitTest1
+    public class CubeTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CalculateFirstNumber()
         {
+            Assert.AreEqual(192, GetCubeEndingInEightEightEight(1));
+        }
+        decimal GetCubeEndingInEightEightEight(int k)
+        {
+            decimal sum = 0;
+            decimal cube = 1;
+            decimal i = 192;
+            while (i > 0)
+            {
+                cube = i * i * i;
+                decimal lastThree = cube % 1000;
+                if (lastThree == 888)
+                {
+                    sum += lastThree;
+                }
+                if (sum / k == 888)
+                {
+                    break;
+                }
+                i = i + 1;
+            }
+            return i;
         }
     }
 }
