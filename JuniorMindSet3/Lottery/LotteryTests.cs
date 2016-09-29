04b6be4fce50cd14a601651876ee5d38cfbf8e2f
ovidiujurje <ovidiujurje@gmail.com>
2016-09-29 15:19:28 +0300

    Calculate probability for 6 out of 49 and 5 out of 49

--- JuniorMindSet3/Lottery/LotteryTests.cs
+++ JuniorMindSet3/Lottery/LotteryTests.cs
@@ -4,11 +4,29 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Lottery
 {
     [TestClass]
-    public class UnitTest1
+    public class LotteryTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SixOutOfFourtyNine()
         {
+            Assert.AreEqual(0.0000000715112384201851626194m, CalculateProbabilityToWin(6, 49));
         }
+        [TestMethod]
+        public void FiveOutOfFourtyNine()
+        {
+            Assert.AreEqual(0.0000005244157484146911925424m, CalculateProbabilityToWin(5, 49));
+        }
+        decimal CalculateProbabilityToWin(int pickedSet, int totalSet)
+        {
+            decimal probability = 1;
+            for (decimal i = 1; i <= pickedSet; i++)
+            {
+                probability = probability * (i / totalSet);
+                totalSet = totalSet - 1;
+            }
+            decimal q = probability + 1;
+            return probability;
+        }
+
     }
 }
