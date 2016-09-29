efc41f11de34ca050a2b3969a5e5749c622b0307
ovidiujurje <ovidiujurje@gmail.com>
2016-09-29 21:50:51 +0300

    Calculate number of days in which we will meet if I go every 6 days and him every 4

--- JuniorMindSet3/Lunch/LunchTests.cs
+++ JuniorMindSet3/Lunch/LunchTests.cs
@@ -4,11 +4,25 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Lunch
 {
     [TestClass]
-    public class UnitTest1
+    public class LunchTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void MeEverySixHimEveryFourDays()
         {
+            Assert.AreEqual(12, CalculateInHowManyDaysYouWillMeetYourFriendAgain(6, 4));
+        }
+        int CalculateInHowManyDaysYouWillMeetYourFriendAgain (int myFrequency, int hisFrequency)
+        {
+            int numberOfDaysuntilWeMeet = 1;
+            while (numberOfDaysuntilWeMeet >= 1)
+            {
+                if (numberOfDaysuntilWeMeet % myFrequency == 0 && numberOfDaysuntilWeMeet % hisFrequency == 0)
+                {
+                    break;
+                }
+                numberOfDaysuntilWeMeet = numberOfDaysuntilWeMeet + 1;
+            }
+            return numberOfDaysuntilWeMeet;
         }
     }
 }
