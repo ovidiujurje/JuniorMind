bc72ce8b259a906719155909632267154c488064
ovidiujurje <ovidiujurje@gmail.com>
2016-09-29 18:13:29 +0300

    Calculate anagrams for "abb", "abbb", "aabb"

--- JuniorMindSet3/Anagrams/AnagramsTests.cs
+++ JuniorMindSet3/Anagrams/AnagramsTests.cs
@@ -4,11 +4,50 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Anagrams
 {
     [TestClass]
-    public class UnitTest1
+    public class AnagramsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CountAnagrams1()
         {
+            Assert.AreEqual(3, NumberOfAnagrams("abb"));
+        }
+        [TestMethod]
+        public void CountAnagrams2()
+        {
+            Assert.AreEqual(4, NumberOfAnagrams("abbb"));
+        }
+        [TestMethod]
+        public void CountAnagrams3()
+        {
+            Assert.AreEqual(6, NumberOfAnagrams("aabb"));
+        }
+
+        int NumberOfAnagrams(string word)
+        {
+            char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            int[] counter = new int[26];
+            int permutations = factorial(word.Length);
+            for (int i = 0; i < word.Length; i++)
+                {
+                if (word[i] == alphabet[word[i] - 'a'])
+                    counter[word[i] - 'a'] = counter[word[i] - 'a'] +1;
+                }
+            int produce = 1;
+            foreach (int nr in counter)
+            {
+                produce *= factorial(nr);
+            }
+            return permutations / produce;
+        }
+        int factorial(int n)
+        {
+            int m;
+            int f = 1;
+            for (m = 1; m <= n; m++)
+            {
+                f = f * m;
+            }
+            return f;
         }
     }
 }
