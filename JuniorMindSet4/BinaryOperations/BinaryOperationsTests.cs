dbade78b71624370cb58f602543b2b2a144c6d57
ovidiujurje <ovidiujurje@gmail.com>
2016-10-03 21:30:29 +0300

    Convert 3, 5 and 49 to binary

--- JuniorMindSet4/BinaryOperations/BinaryOperationsTests.cs
+++ JuniorMindSet4/BinaryOperations/BinaryOperationsTests.cs
@@ -4,11 +4,40 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace BinaryOperations
 {
     [TestClass]
-    public class UnitTest1
+    public class BinaryOperationsTests
     {
         [TestMethod]
         public void TestMethod1()
         {
+            CollectionAssert.AreEqual(new byte[] { 1, 1 }, ConvertNumberFromDecimalToBinary(3));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 0, 1 }, ConvertNumberFromDecimalToBinary(5));
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 1, 0, 0, 0, 1 }, ConvertNumberFromDecimalToBinary(49));
+        }
+        byte[] ConvertNumberFromDecimalToBinary(byte number)
+        {
+            byte[] binaryRepresenationOfNumber = new byte[0];
+            byte numberCopy = number;
+            while (number > 0)
+            {
+                byte remainder = (byte)(numberCopy % 2);
+                Array.Resize(ref binaryRepresenationOfNumber, binaryRepresenationOfNumber.Length + 1);
+                binaryRepresenationOfNumber[binaryRepresenationOfNumber.Length - 1] = remainder;
+                numberCopy /= 2;
+                if (numberCopy == 0)
+                {
+                    break;
+                }
+            }
+            Array.Reverse(binaryRepresenationOfNumber);
+            return binaryRepresenationOfNumber;
         }
     }
 }
