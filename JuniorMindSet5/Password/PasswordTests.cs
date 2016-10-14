bac31ddbe36689a601b7588430ac392e2a42727f
ovidiujurje <ovidiujurje@gmail.com>
2016-10-14 20:24:35 +0300

    Generated random password containing lower case letters

--- JuniorMindSet5/Password/PasswordTests.cs
+++ JuniorMindSet5/Password/PasswordTests.cs
@@ -4,11 +4,23 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Password
 {
     [TestClass]
-    public class UnitTest1
+    public class PasswordTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GenerateRandomPasswordContainingLowerCaseLetters()
         {
+            Assert.AreNotEqual("abcde", GeneratePassword(5));
+        }
+        public enum LowerCaseLetters { a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z }
+        string GeneratePassword(int passwordLength)
+        {
+            string password = string.Empty;
+            Random randomCharacter = new Random();
+            for (int i = 0; i <= passwordLength; i++)
+            {
+                password += (LowerCaseLetters)randomCharacter.Next(26);
+            }
+            return password;
         }
     }
 }
