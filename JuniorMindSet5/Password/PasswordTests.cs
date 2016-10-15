c17476df7c0b633438c1f38634241570c5cc723a
ovidiujurje <ovidiujurje@gmail.com>
2016-10-15 16:13:09 +0300

    Include given number of digits in password

--- JuniorMindSet5/Password/PasswordTests.cs
+++ JuniorMindSet5/Password/PasswordTests.cs
@@ -9,24 +9,28 @@ namespace Password
         [TestMethod]
         public void GenerateRandomPasswordContainingLowerCaseLetters()
         {
-            Assert.AreNotEqual("abcde", GeneratePassword(5, 2));
+            Assert.AreNotEqual("abcde", GeneratePassword(7, 2, 3));
         }
-        string GeneratePassword(int passwordLength, int numberOfUpperCaseLetters)
+        string GeneratePassword(int passwordLength, int numberOfUpperCaseLetters, int numberOfDigits)
         {
             char[] lowerCaseLetters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
             char[] upperCaseLetters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
             string password = string.Empty;
             char[] passwordCharacters = new char[passwordLength];
-            char[] passwordRandomizedCharacters = new char[passwordLength];
             int[] randomIndexes = new int[passwordLength];
             Random randomNumber = new Random();
-            for (int i = 0; i < passwordLength - numberOfUpperCaseLetters; i++)
+            for (int i = 0; i < passwordLength - numberOfUpperCaseLetters - numberOfDigits; i++)
             {
-                passwordCharacters[i] = lowerCaseLetters[randomNumber.Next(25)];
+                passwordCharacters[i] = lowerCaseLetters[randomNumber.Next(lowerCaseLetters.Length)];
             }
-            for (int i = passwordLength - numberOfUpperCaseLetters; i < passwordLength; i++)
+            for (int i = passwordLength - numberOfUpperCaseLetters - numberOfDigits; i < passwordLength - numberOfDigits; i++)
             {
-                passwordCharacters[i] = upperCaseLetters[randomNumber.Next(25)];
+                passwordCharacters[i] = upperCaseLetters[randomNumber.Next(upperCaseLetters.Length)];
+            }
+            for (int i = passwordLength - numberOfDigits; i < passwordLength; i++)
+            {
+                passwordCharacters[i] = digits[randomNumber.Next(digits.Length)];
             }
             for (int i = 0; i < passwordLength; i++)
             {
