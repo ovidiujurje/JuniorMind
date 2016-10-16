8a5c15952064e2007412871068244a966f6c8b85
ovidiujurje <ovidiujurje@gmail.com>
2016-10-16 15:20:50 +0300

    Calculate total distance covered by all cyclists

--- JuniorMindSet5/Cyclometer/CyclometerTests.cs
+++ JuniorMindSet5/Cyclometer/CyclometerTests.cs
@@ -4,11 +4,40 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Cyclometer
 {
     [TestClass]
-    public class UnitTest1
+    public class CyclometerTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldCalculateTotalDistanceCovered()
         {
+            var cyclists = new Cyclist[] { new Cyclist("Walter White", 0.6, new double[] { 1, 2, 3, 4, 5 }), new Cyclist("Thomas Thatch", 0.7, new double[] { 1, 2, 5, 5, 7 }), new Cyclist("Franklin Fane", 0.8, new double[] { 1, 2, 2, 8, 6 }) };
+            Assert.AreEqual(38.2, CalculateTotalDistanceCovered(cyclists));
+        }
+        struct Cyclist
+        {
+            public string name;
+            public double diameter;
+            public double[] rotationsEachSecond;
+            public Cyclist( string name, double diameter, double[] rotationsEachSecond)
+            {
+            this.name = name;
+            this.diameter = diameter;
+            this.rotationsEachSecond = rotationsEachSecond;
+        }
+        }
+
+        double CalculateTotalDistanceCovered(Cyclist[] cyclists)
+        {
+            double result = 0;
+            foreach (Cyclist cyclist in cyclists)
+            {
+                double sum = 0;
+                foreach (double rotation in cyclist.rotationsEachSecond)
+                {
+                    sum += rotation;
+                }
+                result += cyclist.diameter * sum;
+            }
+            return result;
         }
     }
 }
