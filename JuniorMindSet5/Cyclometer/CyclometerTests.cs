b7f10e7727e26df977d3aa31d8d8f1092a96e4d0
ovidiujurje <ovidiujurje@gmail.com>
2016-10-16 16:08:10 +0300

    Get fastest name and second

--- JuniorMindSet5/Cyclometer/CyclometerTests.cs
+++ JuniorMindSet5/Cyclometer/CyclometerTests.cs
@@ -12,6 +12,12 @@ namespace Cyclometer
             var cyclists = new Cyclist[] { new Cyclist("Walter White", 0.6, new double[] { 1, 2, 3, 4, 5 }), new Cyclist("Thomas Thatch", 0.7, new double[] { 1, 2, 5, 5, 7 }), new Cyclist("Franklin Fane", 0.8, new double[] { 1, 2, 2, 8, 6 }) };
             Assert.AreEqual(38.2, CalculateTotalDistanceCovered(cyclists));
         }
+        [TestMethod]
+        public void ShouldGetFastestNameAndSecond()
+        {
+            var cyclists = new Cyclist[] { new Cyclist("Walter White", 0.6, new double[] { 1, 2, 3, 4, 5 }), new Cyclist("Thomas Thatch", 0.7, new double[] { 1, 2, 5, 5, 7 }), new Cyclist("Franklin Fane", 0.8, new double[] { 1, 2, 2, 8, 6 }) };
+            Assert.AreEqual("The fastest cyclist was Franklin Fane in the 4th second", GetFastestSecondAndCyclistName(cyclists));
+        }
         struct Cyclist
         {
             public string name;
@@ -38,6 +44,23 @@ namespace Cyclometer
                 result += cyclist.diameter * sum;
             }
             return result;
+        }
+        string GetFastestSecondAndCyclistName(Cyclist[] cyclists)
+        {
+            double resultSecond = 0;
+            string resultName = string.Empty;
+            foreach (Cyclist cyclist in cyclists)
+            {
+                for (int i = 1; i < cyclist.rotationsEachSecond.Length; i++)
+                {
+                    if (cyclist.rotationsEachSecond[i] >= cyclist.rotationsEachSecond[i - 1])
+                    {
+                        resultSecond = i + 1;
+                        resultName = cyclist.name;
+                    }
+                }
+            }
+            return "The fastest cyclist was " + resultName + " in the " + resultSecond + "th second";
         }
     }
 }
