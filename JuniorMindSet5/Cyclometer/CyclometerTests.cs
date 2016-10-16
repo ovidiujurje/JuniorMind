70f44b79c2af042fd3d1622e4ee76eb48833f9d2
ovidiujurje <ovidiujurje@gmail.com>
2016-10-16 17:15:27 +0300

    Get cyclist with best mean speed

--- JuniorMindSet5/Cyclometer/CyclometerTests.cs
+++ JuniorMindSet5/Cyclometer/CyclometerTests.cs
@@ -18,6 +18,12 @@ namespace Cyclometer
             var cyclists = new Cyclist[] { new Cyclist("Walter White", 0.6, new double[] { 1, 2, 3, 4, 5 }), new Cyclist("Thomas Thatch", 0.7, new double[] { 1, 2, 5, 5, 7 }), new Cyclist("Franklin Fane", 0.8, new double[] { 1, 2, 2, 8, 6 }) };
             Assert.AreEqual("The fastest cyclist was Franklin Fane in the 4th second", GetFastestSecondAndCyclistName(cyclists));
         }
+        [TestMethod]
+        public void ShouldGetCyclistWithBestMeanSpeed()
+        {
+            var cyclists = new Cyclist[] { new Cyclist("Walter White", 0.6, new double[] { 1, 2, 3, 4, 5 }), new Cyclist("Thomas Thatch", 0.7, new double[] { 1, 2, 5, 5, 7 }), new Cyclist("Franklin Fane", 0.8, new double[] { 1, 2, 2, 8, 6 }) };
+            Assert.AreEqual("Franklin Fane", GetCyclistWithBestMeanSpeed(cyclists));
+        }
         struct Cyclist
         {
             public string name;
@@ -48,7 +54,7 @@ namespace Cyclometer
         string GetFastestSecondAndCyclistName(Cyclist[] cyclists)
         {
             double resultSecond = 0;
-            string resultName = string.Empty;
+            Cyclist resultCyclist = cyclists[0];
             foreach (Cyclist cyclist in cyclists)
             {
                 for (int i = 1; i < cyclist.rotationsEachSecond.Length; i++)
@@ -56,11 +62,35 @@ namespace Cyclometer
                     if (cyclist.rotationsEachSecond[i] >= cyclist.rotationsEachSecond[i - 1])
                     {
                         resultSecond = i + 1;
-                        resultName = cyclist.name;
+                        resultCyclist = cyclist;
                     }
                 }
             }
-            return "The fastest cyclist was " + resultName + " in the " + resultSecond + "th second";
+            return "The fastest cyclist was " + resultCyclist.name + " in the " + resultSecond + "th second";
+        }
+        string GetCyclistWithBestMeanSpeed(Cyclist[] cyclists)
+        {
+            double[] meanSpeed = new double[cyclists.Length];
+            int i = 0;
+            foreach (Cyclist cyclist in cyclists)
+            {
+                double sumSpeed = 0;
+                foreach (double rotation in cyclist.rotationsEachSecond)
+                {
+                    sumSpeed += rotation * cyclist.diameter;
+                }
+                meanSpeed[i] = sumSpeed / cyclist.rotationsEachSecond.Length;
+                i++;
+            }
+            int resultPosition = 0;
+            for (int j = 1; j < meanSpeed.Length; j++)
+            {
+                if (meanSpeed[j] >= meanSpeed[j - 1])
+                {
+                    resultPosition = j;
+                }
+            }
+            return cyclists[resultPosition].name;
         }
     }
 }
