d7e2c79415db0ec57d8a757d1247ce4c6b119a71
ovidiujurje <ovidiujurje@gmail.com>
2016-10-20 16:30:35 +0300

    Replace character with string

--- JuniorMindSet6/Replacement/ReplacementTests.cs
+++ JuniorMindSet6/Replacement/ReplacementTests.cs
@@ -9,6 +9,27 @@ namespace Replacement
         [TestMethod]
         public void ReplaceCharacterWithString()
         {
+            Assert.AreEqual("AreplacementBreplacementC", ReplaceCharacetrWithString("AxBxC", 'x', "replacement"));
+        }
+        string ReplaceCharacetrWithString(string givenString, char givenCharacter, string characterRepalcement)
+        {
+            for (int i = 0; i < givenString.Length; i++)
+            {
+                if (givenString[i] == givenCharacter)
+                {
+                    givenString = Substring(givenString, 0, i - 1) + characterRepalcement + Substring(givenString, i + 1, givenString.Length - 1);
+                    i += characterRepalcement.Length - 1;
+                }
+            }
+
+            return givenString;
+        }
+        string Substring(string theString, int startPosition, int endPosition)
+        {
+            string newString = string.Empty;
+            for (int i = startPosition; i <= endPosition; i++)
+                newString += theString[i];
+            return newString;
         }
     }
 }
