00f78942cfc9b5ef9b71422ec0fe62e9d5a22c13
ovidiujurje <ovidiujurje@gmail.com>
2016-10-20 15:54:56 +0300

    Invert string

--- JuniorMindSet6/Inversion/InversionTests.cs
+++ JuniorMindSet6/Inversion/InversionTests.cs
@@ -4,11 +4,24 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Inversion
 {
     [TestClass]
-    public class UnitTest1
+    public class InversionTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InvertString()
         {
+            Assert.AreEqual("dcba", InvertString("abcd"));
+        }
+        string InvertString(string givenString)
+        {
+            if (givenString.Length == 0) return givenString;
+            return givenString[givenString.Length - 1] + InvertString(RemoveLastCharacterFromString(givenString));
+        }
+        string RemoveLastCharacterFromString(string theString)
+        {
+            string newString = string.Empty;
+            for (int i = 0; i < theString.Length - 1; i++)
+                newString += theString[i];
+            return newString;
         }
     }
 }
