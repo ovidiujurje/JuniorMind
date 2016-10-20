5978e8ffec2592229f3261b713e7e63fe6ad039e
ovidiujurje <ovidiujurje@gmail.com>
2016-10-20 14:45:39 +0300

    Calculate kth Fibonacci number

--- JuniorMindSet6/Fibonacci/FibonacciTests.cs
+++ JuniorMindSet6/Fibonacci/FibonacciTests.cs
@@ -4,11 +4,30 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Fibonacci
 {
     [TestClass]
-    public class UnitTest1
+    public class FibonacciTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SeventhFibonacciNumber()
         {
+            Assert.AreEqual(8, Fibonacci(7));
+        }
+        [TestMethod]
+        public void NinethFibonacciNumber()
+        {
+            Assert.AreEqual(21, Fibonacci(9));
+        }
+        int Fibonacci(int k)
+        {
+            int previous = 0;
+            Fibonacci(k, ref previous);
+            return previous;
+        }
+        int Fibonacci(int k, ref int previous)
+        {
+            if (k < 2) return k;
+            int beforePrevious = 0;
+            previous = Fibonacci(k - 1, ref beforePrevious);
+            return previous + beforePrevious;
         }
     }
 }
