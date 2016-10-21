04d7d662ae2b6cf37ca22ccdad4ccd4925807a2e
ovidiujurje <ovidiujurje@gmail.com>
2016-10-21 23:20:00 +0300

    Get moves for 3 discs

--- JuniorMindSet6/TowersOfHanoi/TowersOfHanoiTests.cs
+++ JuniorMindSet6/TowersOfHanoi/TowersOfHanoiTests.cs
@@ -4,11 +4,23 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace TowersOfHanoi
 {
     [TestClass]
-    public class UnitTest1
+    public class TowersOfHanoiTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetFirstTowerWithAllDiscs()
         {
+            string response = "";
+            MoveDiscsFromTowerAToTowerC(3, "A", "B", "C", ref response);
+            Assert.AreEqual("A->C|A->B|C->B|A->C|B->A|B->C|A->C|", response);
+        }
+        void MoveDiscsFromTowerAToTowerC(int numberOfDiscs, string towerA, string towerB, string towerC, ref string solution)
+        {
+            if (numberOfDiscs > 0)
+            {
+                MoveDiscsFromTowerAToTowerC(numberOfDiscs - 1, towerA, towerC, towerB, ref solution);
+                solution += towerA + "->" + towerC + "|";
+                MoveDiscsFromTowerAToTowerC(numberOfDiscs - 1, towerB, towerA, towerC, ref solution);
+            }
         }
     }
 }
