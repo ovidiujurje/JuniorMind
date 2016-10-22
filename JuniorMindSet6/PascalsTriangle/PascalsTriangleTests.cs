6c51256fdffac3972d38cb7112e41db7529f874f
ovidiujurje <ovidiujurje@gmail.com>
2016-10-22 16:14:47 +0300

    Generate Pascal's Triangle for level 3

--- JuniorMindSet6/PascalsTriangle/PascalsTriangleTests.cs
+++ JuniorMindSet6/PascalsTriangle/PascalsTriangleTests.cs
@@ -4,11 +4,30 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace PascalsTriangle
 {
     [TestClass]
-    public class UnitTest1
+    public class PascalsTriangleTests
     {
         [TestMethod]
         public void TestMethod1()
         {
+            CollectionAssert.AreEqual(new int[,] { {1, 0, 0 }, { 1, 1, 0 }, { 1, 2, 1 } }, GeneratePascalsTriangle(3));
+        }
+        int[,] GeneratePascalsTriangle (int level)
+        {
+            int[,] triangle = new int[level, level];
+            for (int i = 0; i < level; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    triangle[i, j] = GenerateLine(i, j);
+                }
+            }
+            return triangle;
+        }
+        int GenerateLine(int row, int col)
+        {
+            if (col == 0 || col == row) return 1;
+            if (row == 0 || row == 1) return 1;
+            return GenerateLine(row - 1, col - 1) + GenerateLine(row - 1, col);
         }
     }
 }
