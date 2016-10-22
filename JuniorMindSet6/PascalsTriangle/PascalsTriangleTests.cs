d35b71d4352a5611d51e9b5942e94d4e550ee382
ovidiujurje <ovidiujurje@gmail.com>
2016-10-22 16:39:06 +0300

    Generate Pascal's Triangle for level 7
    Also:
    Change GenerateLine function name to GenerateElement.
    Remove unnecessary "if" statement from GenerateElement function..

--- JuniorMindSet6/PascalsTriangle/PascalsTriangleTests.cs
+++ JuniorMindSet6/PascalsTriangle/PascalsTriangleTests.cs
@@ -7,9 +7,22 @@ namespace PascalsTriangle
     public class PascalsTriangleTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void LevelThreeTriangle()
         {
-            CollectionAssert.AreEqual(new int[,] { {1, 0, 0 }, { 1, 1, 0 }, { 1, 2, 1 } }, GeneratePascalsTriangle(3));
+            CollectionAssert.AreEqual(new int[,] { { 1, 0, 0 },
+                                                   { 1, 1, 0 },
+                                                   { 1, 2, 1 } }, GeneratePascalsTriangle(3));
+        }
+        [TestMethod]
+        public void LevelSevenTriangle()
+        {
+            CollectionAssert.AreEqual(new int[,] { { 1, 0, 0, 0, 0, 0, 0 },
+                                                   { 1, 1, 0, 0, 0, 0, 0 },
+                                                   { 1, 2, 1, 0, 0, 0, 0 },
+                                                   { 1, 3, 3, 1, 0, 0, 0 },
+                                                   { 1, 4, 6, 4, 1, 0, 0 },
+                                                   { 1, 5, 10, 10, 5, 1, 0 },
+                                                   { 1, 6, 15, 20, 15, 6, 1 } }, GeneratePascalsTriangle(7));
         }
         int[,] GeneratePascalsTriangle (int level)
         {
@@ -18,16 +31,15 @@ namespace PascalsTriangle
             {
                 for (int j = 0; j < i + 1; j++)
                 {
-                    triangle[i, j] = GenerateLine(i, j);
+                    triangle[i, j] = GenerateElement(i, j);
                 }
             }
             return triangle;
         }
-        int GenerateLine(int row, int col)
+        int GenerateElement(int row, int col)
         {
             if (col == 0 || col == row) return 1;
-            if (row == 0 || row == 1) return 1;
-            return GenerateLine(row - 1, col - 1) + GenerateLine(row - 1, col);
+            return GenerateElement(row - 1, col - 1) + GenerateElement(row - 1, col);
         }
     }
 }
