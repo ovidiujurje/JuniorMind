935d04bc5ccbed6d7f52d544b56738f8b902b315
ovidiujurje <ovidiujurje@gmail.com>
2016-10-24 21:36:31 +0300

    Sort numbers ascending

--- JuniorMindSet7/LottoExtraction/LottoExtractionTests.cs
+++ JuniorMindSet7/LottoExtraction/LottoExtractionTests.cs
@@ -4,11 +4,35 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace LottoExtraction
 {
     [TestClass]
-    public class UnitTest1
+    public class LottoExtractionTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SortNumbersAscending()
         {
+            CollectionAssert.AreEqual( new byte[] { 4, 7, 18, 25, 39, 42 }, SortNumbersAscending(new byte[] { 7, 4, 18, 39, 42, 25 }));
+        }
+        byte[] SortNumbersAscending(byte[] lottoNumbers)
+        {
+            for (int i = 1; i < lottoNumbers.Length; i++)
+            {
+                DoInsertion(ref lottoNumbers, i, i - 1);
+            }
+            return lottoNumbers;
+        }
+        void DoInsertion(ref byte[] lottonumbers, int index, int previousIndex)
+        {
+            if (previousIndex < 0) return;
+            if (lottonumbers[index] < lottonumbers[previousIndex])
+            {
+                Swap(ref lottonumbers[index], ref lottonumbers[previousIndex]);
+                DoInsertion(ref lottonumbers, index - 1, previousIndex - 1);
+            }
+        }
+        void Swap (ref byte first,ref  byte second)
+        {
+            byte temp = first;
+            first = second;
+            second = temp;
         }
     }
 }
