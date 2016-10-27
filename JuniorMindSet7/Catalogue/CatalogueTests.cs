6593d9346ff5797b448f14d14bb21b9b3afaa3e0
ovidiujurje <ovidiujurje@gmail.com>
2016-10-27 14:46:45 +0300

    Sort students descending by general mean (Selection)

--- JuniorMindSet7/Catalogue/CatalogueTests.cs
+++ JuniorMindSet7/Catalogue/CatalogueTests.cs
@@ -9,14 +9,37 @@ namespace Catalogue
         [TestMethod]
         public void SortStudentsAlphabeticallyUsingBubbleSort()
         {
-            Student three = new Student("Chereches Voicu", new int[] { 10 }, new int[] { 10, 9 }, new int[] { 10 }, new int[] { 9, 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 });
-            Student two = new Student("Brete Origen", new int[] { 5 }, new int[] { 6 }, new int[] { 5 }, new int[] { 5 }, new int[] { 6 }, new int[] { 6, 5 }, new int[] { 7 }, new int[] { 6 }, new int[] { 6 }, new int[] { 5 }, new int[] { 9 }, new int[] { 7 });
-            Student one = new Student("Chira Iulia", new int[] { 9 }, new int[] { 9 }, new int[] { 9 }, new int[] { 9, 8, 7 }, new int[] { 9 }, new int[] { 6 }, new int[] { 8, 9 }, new int[] { 6 }, new int[] { 6 }, new int[] { 10 }, new int[] { 9 }, new int[] { 7 });
+            Student three = new Student("Chereches Voicu", 0, new int[] { 10 }, new int[] { 10, 9 }, new int[] { 10 }, new int[] { 9, 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 });
+            Student two = new Student("Brete Origen", 0, new int[] { 5 }, new int[] { 6 }, new int[] { 5 }, new int[] { 5 }, new int[] { 6 }, new int[] { 6, 5 }, new int[] { 7 }, new int[] { 6 }, new int[] { 6 }, new int[] { 5 }, new int[] { 9 }, new int[] { 7 });
+            Student one = new Student("Chira Iulia", 0, new int[] { 9 }, new int[] { 9 }, new int[] { 9 }, new int[] { 9, 8, 7 }, new int[] { 9 }, new int[] { 6 }, new int[] { 8, 9 }, new int[] { 6 }, new int[] { 6 }, new int[] { 10 }, new int[] { 9 }, new int[] { 7 });
             CollectionAssert.AreEqual(new Student[] { two, three, one }, SortStudentsAlphabeticallyBubble(new Student[] { one, three, two }));
+        }
+        [TestMethod]
+        public void ShouldCalculateGeneralMeans()
+        {
+            Student three = new Student("Chereches Voicu", 0, new int[] { 10 }, new int[] { 10, 9 }, new int[] { 10 }, new int[] { 9, 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 });
+            Student two = new Student("Brete Origen", 0, new int[] { 5 }, new int[] { 6 }, new int[] { 5 }, new int[] { 5 }, new int[] { 6 }, new int[] { 6, 5 }, new int[] { 7 }, new int[] { 6 }, new int[] { 6 }, new int[] { 5 }, new int[] { 9 }, new int[] { 7 });
+            Student one = new Student("Chira Iulia", 0, new int[] { 9 }, new int[] { 9 }, new int[] { 9 }, new int[] { 9, 8, 7 }, new int[] { 9 }, new int[] { 6 }, new int[] { 8, 9 }, new int[] { 6 }, new int[] { 6 }, new int[] { 10 }, new int[] { 9 }, new int[] { 7 });
+            Student[] students = { three, two, one };
+            CalculateGeneralMeans(ref students);
+            Assert.AreEqual(9.9166666666666661, students[0].mean);
+            Assert.AreEqual(6.041666666666667, students[1].mean);
+            Assert.AreEqual(8.0416666666666661, students[2].mean);
+        }
+        [TestMethod]
+        public void SortStudentsByGeneralMeanDescendingSelectionSort()
+        {
+            Student three = new Student("Chereches Voicu", 0, new int[] { 10 }, new int[] { 10, 9 }, new int[] { 10 }, new int[] { 9, 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 }, new int[] { 10 });
+            Student two = new Student("Brete Origen", 0, new int[] { 5 }, new int[] { 6 }, new int[] { 5 }, new int[] { 5 }, new int[] { 6 }, new int[] { 6, 5 }, new int[] { 7 }, new int[] { 6 }, new int[] { 6 }, new int[] { 5 }, new int[] { 9 }, new int[] { 7 });
+            Student one = new Student("Chira Iulia", 0, new int[] { 9 }, new int[] { 9 }, new int[] { 9 }, new int[] { 9, 8, 7 }, new int[] { 9 }, new int[] { 6 }, new int[] { 8, 9 }, new int[] { 6 }, new int[] { 6 }, new int[] { 10 }, new int[] { 9 }, new int[] { 7 });
+            Student[] students = { three, one, two };
+            CalculateGeneralMeans(ref students);
+            CollectionAssert.AreEqual(students, SortStudentsByGeneralMeanDescendingSelectionSort(new Student[] { two, one, three }));
         }
         public struct Student
         {
             public string name;
+            public double mean;
             public int[] matterOne;
             public int[] matterTwo;
             public int[] matterThree;
@@ -29,9 +52,10 @@ namespace Catalogue
             public int[] matterTen;
             public int[] matterEleven;
             public int[] matterTwelve;
-            public Student( string name, int[] matterOne, int[] matterTwo, int[] matterThree, int[] matterFour, int[] matterFive, int[] matterSix, int[] matterSeven, int[] matterEight, int[] matterNine, int[] matterTen, int[] matterEleven, int[] matterTwelve)
+            public Student( string name, double mean, int[] matterOne, int[] matterTwo, int[] matterThree, int[] matterFour, int[] matterFive, int[] matterSix, int[] matterSeven, int[] matterEight, int[] matterNine, int[] matterTen, int[] matterEleven, int[] matterTwelve)
             {
                 this.name = name;
+                this.mean = mean;
                 this.matterOne = matterOne;
                 this.matterTwo = matterTwo;
                 this.matterThree = matterThree;
@@ -64,6 +88,41 @@ namespace Catalogue
                 }
             }
             return catalogue;
+        }
+        Student[] SortStudentsByGeneralMeanDescendingSelectionSort(Student[] catalogue)
+        {
+            CalculateGeneralMeans(ref catalogue);
+            for (int i = 0; i < catalogue.Length; i++)
+            {
+                Swap(ref catalogue[i], ref catalogue[MaxValueIndex(catalogue, i)]);
+            }
+            return catalogue;
+        }
+        void CalculateGeneralMeans(ref Student[] catalogue)
+        {
+            for (int i = 0; i < catalogue.Length; i++)
+            {
+                catalogue[i].mean = (Mean(catalogue[i].matterOne) + Mean(catalogue[i].matterTwo) + Mean(catalogue[i].matterThree) + Mean(catalogue[i].matterFour) + Mean(catalogue[i].matterFive) + Mean(catalogue[i].matterSix) + Mean(catalogue[i].matterSeven) + Mean(catalogue[i].matterEight) + Mean(catalogue[i].matterNine) + Mean(catalogue[i].matterTen) + Mean(catalogue[i].matterEleven) + Mean(catalogue[i].matterTwelve)) / 12;
+            }
+        }
+        double Mean(int[] array)
+        {
+            double sum = 0;
+            foreach (int value in array)
+                sum += value;
+            double mean = sum / array.Length;
+            return mean;
+        }
+        int MaxValueIndex(Student[] array, int startIndex)
+        {
+            int maxIndex = startIndex;
+            double max = array[startIndex].mean;
+            for (int i = startIndex + 1; i < array.Length; i++)
+            {
+                if (array[i].mean > max)
+                    maxIndex = i;
+            }
+            return maxIndex;
         }
         void Swap(ref Student first, ref Student second)
         {
