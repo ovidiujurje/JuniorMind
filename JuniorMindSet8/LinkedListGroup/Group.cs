d3d45720549011855fc5d7d50e0f98657f275b4e
ovidiujurje <ovidiujurje@gmail.com>
2016-11-15 15:03:03 +0200

    linkedlist project

---
+++ JuniorMindSet8/LinkedListGroup/Group.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListGroup
+{
+    public class Group<T>: IEnumerable<T>
+    {
+        private LinkedList<T> list;
+
+        public Group(T[] array)
+        {
+            list = new LinkedList<T>(array);
+        }
+
+        public void AddFirst(T value)
+        {
+            list.AddFirst(value);
+        }
+
+        public void AddLast(T value)
+        {
+            list.AddLast(value);
+        }
+
+        public void Insert(T place, T value)
+        {
+            LinkedListNode<T> current = list.Find(place);
+            list.AddBefore(current, value);
+        }
+
+        public void RemoveFirst()
+        {
+            list.RemoveFirst();
+        }
+
+        public void RemoveLast()
+        {
+            list.RemoveLast();
+        }
+
+        public void Remove(T value)
+        {
+            list.Remove(value);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)list).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)list).GetEnumerator();
+        }
+    }
+}
---
+++ JuniorMindSet8/LinkedListGroup/GroupTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace LinkedListGroup
+{
+    public class GroupTests
+    {
+        [Fact]
+        public void AddFirst()
+        {
+            Group<string> set = new Group<string>(new string[] { "one", "two", "three" });
+            set.AddFirst("four");
+            Assert.Equal(new string[] { "four", "one", "two", "three" }, set);
+        }
+        [Fact]
+        public void AddLast()
+        {
+            Group<string> set = new Group<string>(new string[] { "one", "two", "three" });
+            set.AddLast("four");
+            Assert.Equal(new string[] { "one", "two", "three", "four" }, set);
+        }
+        [Fact]
+        public void InsertBefore()
+        {
+            Group<string> set = new Group<string>(new string[] { "one", "two", "three" });
+            set.Insert("two", "four");
+            Assert.Equal(new string[] {  "one", "four", "two", "three" }, set);
+        }
+        [Fact]
+        public void RemoveFirst()
+        {
+            Group<string> set = new Group<string>(new string[] { "one", "two", "three" });
+            set.RemoveFirst();
+            Assert.Equal(new string[] { "two", "three" }, set);
+        }
+        [Fact]
+        public void RemoveLast()
+        {
+            Group<string> set = new Group<string>(new string[] { "one", "two", "three" });
+            set.RemoveLast();
+            Assert.Equal(new string[] { "one", "two"}, set);
+        }
+        [Fact]
+        public void Remove()
+        {
+            Group<string> set = new Group<string>(new string[] { "one", "two", "three" });
+            set.Remove("two");
+            Assert.Equal(new string[] { "one", "three" }, set);
+        }
+    }
+}
