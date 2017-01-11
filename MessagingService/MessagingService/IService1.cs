991fb533edbb0d9634aaa30c1aca7e50b86f4663
ovidiujurje <ovidiujurje@gmail.com>
2017-01-11 15:05:26 +0200

    wcf1

---
+++ MessagingService/MessagingService/IService1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MessagingService
+{
+    [ServiceContract(CallbackContract = typeof(IService1Callback))]
+    public interface IService1
+    {
+        [OperationContract]
+        void GetData(string value);
+    }
+
+    public interface IService1Callback
+    {
+        [OperationContract]
+        void AddMessage(string history);
+    }
+}
---
+++ MessagingService/MessagingService/Service1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MessagingService
+{
+    public class Service1 : IService1
+    {
+        public void GetData(string value)
+        {
+            var history = string.Empty;
+            history += Environment.NewLine + value;
+            OperationContext.Current.GetCallbackChannel<IService1Callback>().AddMessage(history);
+        }
+    }
+}
